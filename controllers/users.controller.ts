4cca85b9e10a7ca2ea724e888f20379078c3a662
chiranjib <chiranjibnath21313@gmail.com>
2023-07-07 02:21:04 +0530

    error fix

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -61,6 +61,8 @@ export const authUser = asyncHandler(async (req, res) => {
 export const logoutUser = asyncHandler(async (req, res) => {
   res.cookie("jwt_user", "", {
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
     expires: new Date(0),
   });
   res.status(200).json({ message: "User Logged Out" });
